dd648b81addf539cc8b71cd03c8f40644726ff47
raizo07 <agadaraizo@gmail.com>
2025-03-24 08:20:41 +0100

    update the mod.rs file

--- crates/air/src/components/lessthan/mod.rs
+++ crates/air/src/components/lessthan/mod.rs
@@ -1,2 +1,5 @@
-pub mod component;
-pub mod table;
+mod component;
+mod table;
+
+pub use component::{LessThanComponent, LessThanEval};
+pub use table::{interaction_trace_evaluation, LessThanColumn, LessThanTable, LessThanTableRow};
