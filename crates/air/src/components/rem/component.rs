2f6626c3cb0e3ddd9376deee869fde8ec456d917
Mahmoud Mohajer <mahmohajer3@gmail.com>
2025-06-23 18:06:12 +0430

    Implementing the evaluation component for Rem operator

---
+++ crates/air/src/components/rem/component.rs
@@ -0,0 +1,116 @@
+use crate::components::{add::component::NodeElements, RemClaim};
+use luminal::prelude::petgraph::adj::OutgoingEdgeIndices;
+use num_traits::One;
+use numerair::eval::EvalFixedPoint;
+use stwo_prover::constraint_framework::{
+    EvalAtRow, FrameworkComponent, FrameworkEval, RelationEntry,
+};
+
+pub type RemComponent = FrameworkComponent<RemEval>;
+
+pub struct RemEval {
+    log_size: u32,
+    node_elements: NodeElements,
+}
+
+impl RemEval {
+    pub fn new(claim: &RemClaim, node_elements: NodeElements) -> Self {
+        Self {
+            log_size: claim.log_size,
+            node_elements,
+        }
+    }
+}
+
+impl FrameworkEval for RemEval {
+    fn log_size(&self) -> u32 {
+        self.log_size
+    }
+
+    fn max_constraint_log_degree_bound(&self) -> u32 {
+        self.log_size + 1
+    }
+
+    fn evaluate<E: EvalAtRow>(&self, mut eval: E) -> E {
+        //IDs
+        let node_id = eval.next_trace_mask();
+        let lhs_id = eval.next_trace_mask();
+        let rhs_id = eval.next_trace_mask();
+        let idx = eval.next_trace_mask();
+        let is_last_idx = eval.next_trace_mask();
+
+        //Next IDs for transition constraints
+        let next_node_id = eval.next_trace_mask();
+        let next_lhs_id = eval.next_trace_mask();
+        let next_rhs_id = eval.next_trace_mask();
+        let next_idx = eval.next_trace_mask();
+
+        // Values for consistency constraints
+        let lhs_val = eval.next_trace_mask();
+        let rhs_val = eval.next_trace_mask();
+        let out_val = eval.next_trace_mask();
+        let rem_val = eval.next_trace_mask();
+
+        // Mulitiplication for interaction constraints
+        let lhs_mult = eval.next_trace_mask();
+        let rhs_mult = eval.next_trace_mask();
+        let out_mult = eval.next_trace_mask();
+
+        // ┌─────────────────────────────┐
+        // │   Consistency Constraints   │
+        // └─────────────────────────────┘
+
+        // The is_last_idx flag is either 0 or 1.
+        eval.add_constraint(is_last_idx.clone() * (is_last_idx.clone() - E::F::one()));
+
+        eval.eval_fixed_rem(
+            lhs_val.clone(),
+            rhs_val.clone(),
+            out_val.clone(),
+            rem_val.clone(),
+        );
+
+        // ┌────────────────────────────┐
+        // │   Transition Constraints   │
+        // └────────────────────────────┘
+
+        // If this is not the last index for this operation, then:
+        // 1. The next row should be for the same operation on the same tensors.
+        // 2. The index should increment by 1.
+        let not_last = E::F::one() - is_last_idx;
+
+        // Same node ID 
+        eval.add_constraint(not_last.clone() * (next_node_id - node_id.clone()));
+
+        // Same Tensor IDs
+        eval.add_constraint(not_last.clone() * (next_lhs_id - lhs_id.clone()));
+        eval.add_constraint(not_last.clone() * (next_lhs_id - lhs_id.clone()));
+
+        // Index increment by 1 
+        eval.add_constraint(not_last * (next_idx - idx - E::F::one()));
+
+        // ┌─────────────────────────────┐
+        // │   Interaction Constraints   │
+        // └─────────────────────────────┘
+
+        eval.add_to_relation(RelationEntry::new(
+            &self.node_elements,
+            lhs_mult.into(),
+            &[lhs_val, lhs_id]));
+        
+        eval.add_to_relation(RelationEntry::new(
+            &self.node_elements,
+            rhs_mult.into(),
+            &[rhs_val, rhs_id]));
+        
+        eval.add_to_relation(RelationEntry::new(
+            &self.node_elements,
+            out_mult.into(),
+            &[out_val, node_id],
+        ));
+
+        eval.finalize_logup();
+
+        eval
+    }
+}
