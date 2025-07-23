f1e17e026820f4ad47c3c8cf5d9347f8c19d8169
Mahmoud Mohajer <mahmohajer3@gmail.com>
2025-07-23 14:08:01 +0430

    Add rem component to log_sum_valid function in utils.rs to support remainder claims

--- crates/air/src/utils.rs
+++ crates/air/src/utils.rs
@@ -50,6 +50,7 @@ pub fn log_sum_valid(interaction_claim: &LuminairInteractionClaim) -> bool {
         &interaction_claim.sin,
         &interaction_claim.sin_lookup,
         &interaction_claim.sqrt,
+        &interaction_claim.rem,
     ] {
         if let Some(ref int_cl) = claim_opt {
             sum += int_cl.claimed_sum.into();
